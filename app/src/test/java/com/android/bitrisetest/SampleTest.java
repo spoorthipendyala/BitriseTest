a5558c7171c188bdce000aa202d38ed03a3a5e81
spoorthipendyala <spoorthipnk@gmail.com>
2018-09-13 14:55:39 -0400

    false test 2

---
+++ app/src/test/java/com/android/bitrisetest/SampleTest.java
@@ -0,0 +1,18 @@
+package com.android.bitrisetest;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+@SmallTest
+public class SampleTest {
+
+    @Test
+    public void this_will_fail(){
+
+        assertEquals(true,false);
+    }
+}
